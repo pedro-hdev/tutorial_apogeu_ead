67244fcbdfe09c28e13672d32bc52e4ddc4786d9
pedro-hdev <pedrophveras@gmail.com>
2025-04-03 09:03:00 -0300

    ++height video

--- website/src/components/video.jsx
+++ website/src/components/video.jsx
@@ -3,7 +3,7 @@ export function Video(){
 return(
 
 <div>
-    <iframe src="https://www.youtube.com/embed/4RNX05yvBRE?autoplay=1&mute=1" className="w-[700px] h-[400px] rounded-3xl border-solid border-4 border-green-800 max-md:w-full max-md:h-[300px] max-sm:h-[250px]"></iframe>
+    <iframe src="https://www.youtube.com/embed/4RNX05yvBRE?autoplay=1&mute=1" className="w-[700px] h-[400px] rounded-3xl border-solid border-4 border-green-800 max-md:w-full max-md:h-[300px] max-sm:h-[500px]"></iframe>
 </div>
 
 )
